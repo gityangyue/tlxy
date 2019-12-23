709144adc4878a95d70a819d037e94eff3541e60
gityangyue <289859714@qq.com>
2019-12-23 21:31:24 +0800

    2019 12 23

---
+++ Yanglab/test1-jisuan.py
@@ -0,0 +1,15 @@
+def jisuan(current_qty=0, current_price=0,goal=0):
+    #数量列表
+    qty = []
+    #价格列表
+    pric = []
+    #总资金列表
+    sum = []
+    #当前得总资金
+    current_sum = current_qty*current_price
+    for qty_i in range(1,10):
+        qty.append(qty_i*100)
+        sum_i =
+
+
+
